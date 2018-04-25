1e16051e68474913a44078fc69a4551f33c1774c
Mohamed Ismail <mhismail@buffalo.edu>
2018-04-24 20:43:31 -0400

    Fix linux transparency bug and update dependencies

--- main.js
+++ main.js
@@ -6,32 +6,28 @@ const {BrowserWindow} = electron;
 const {globalShortcut} = electron;
 const {ipcRenderer} = electron;
 const {webContents} = electron;
-var path = require('path')
 
 
 var mainWindow = null;
 app.commandLine.appendSwitch('--disable-http-cache');
 
-if (process.platform=="darwin"){
-    app.disableHardwareAcceleration()
-}
 
 app.on('ready', function() {
-     protocol.unregisterProtocol('', () => {
+    setTimeout(() => {
     mainWindow = new BrowserWindow({
         frame: false,
         height: 500,
         //resizable: false,
         width: 500,
         transparent:true,
-        icon: path.join(__dirname, 'images/512x512.png')
+  	backgroundColor: '#00000000'
+
     });
     mainWindow.loadURL('file://' + __dirname + '/index.html');
     mainWindow.maximize()
+	},100)
 
-    
- 
-     })
+
 });
 
 
@@ -40,5 +36,6 @@ app.on('ready', function() {
 app.on('window-all-closed', () => {
   app.quit();
 });
+
 
 
